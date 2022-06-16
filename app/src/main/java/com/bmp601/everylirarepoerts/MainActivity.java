c208bb124bff3efa3ea3858617314d543de2e5d8
althabbas11 <drhussamhabbas11@gmail.com>
2022-06-16 13:45:11 +0300

    Added Splash Screen

--- app/src/main/java/com/bmp601/everylirarepoerts/MainActivity.java
+++ app/src/main/java/com/bmp601/everylirarepoerts/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bmp601.everylirarepoerts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,8 +10,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition.
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Keep the splash screen visible for this Activity
+        splashScreen.setKeepOnScreenCondition(() -> true);
+
         Intent intent = new Intent(getApplicationContext(), ReportsActivity.class);
         startActivity(intent);
         finish();
